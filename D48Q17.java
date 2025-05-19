9f1af8d39f96800ce34e36b0e976b9c236ac85d6
itc-s24010 <s24010@std.it-college.ac.jp>
2025-05-19 10:03:36 +0900

    課題

---
+++ D48Q17.java
@@ -0,0 +1,14 @@
+public class D48Q17 {
+    public static void main(String[] args) {
+        double baseValue = 3.5;
+        int units = 3;
+
+        double width = baseValue * units;
+        System.out.println(width);
+    }
+}
+
+
+/*
+D48Q17について
+D48は第48回のことを表しています*/
