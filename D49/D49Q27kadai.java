6cb44372e0f30eb272652032b522e2680fe19a4d
itc-s24010 <s24010@std.it-college.ac.jp>
2025-05-30 10:44:39 +0900

    課題

---
+++ D49/D49Q27kadai.java
@@ -0,0 +1,11 @@
+package D49;
+public class D49Q27kadai {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30};
+        int sum = 0;
+        for (int num : numbers) {
+            sum += num;
+        }
+        System.out.println(sum);
+    }
+}
