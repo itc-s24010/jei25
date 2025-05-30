4d0a75e3984354d186ffbfcf07a87ac884e79938
itc-s24010 <s24010@std.it-college.ac.jp>
2025-05-30 11:38:03 +0900

    課題

---
+++ D49/D50Q20kadai.java
@@ -0,0 +1,10 @@
+package D49;
+
+public class D50Q20kadai {
+    public static void main(String[] args) {
+        int num = 1234;
+        String scale = num > 10000 ? "Wide" : "Narrow";
+
+        System.out.println(scale);
+    }
+}
